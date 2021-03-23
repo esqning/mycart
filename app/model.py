bdc8932552453bbcb759144fbaf7db5370205ce1
esqning <esqning@aliyun.com>
2021-03-23 21:58:28 +0800

    购物车添加MySQL数据库支持

---
+++ app/model.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+from app import db
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    adminId = db.Column(db.String(20))
+    passWd = db.Column(db.String(20))
+    name = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Admin {}>'.format(self.name)
+
+
+class Basket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.String(20))
+    goodsId = db.Column(db.String(20))
+    orderTime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    orderCount = db.Column(db.SmallInteger)
+    salePrice = db.Column(db.Float)
+    userChecked = db.Column(db.Boolean)
+    orderNumber = db.Column(db.String(50))
+    postTime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    adminChecked = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Basket {}>'.format(self.orderNumber)
+
+
+class Goods(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    typeid = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    an_number = db.Column(db.String(50))
+    producer = db.Column(db.String(20))
+    package = db.Column(db.String(20))
+    salePrice = db.Column(db.Float)
+    storePrice = db.Column(db.Float)
+    content = db.Column(db.String(1000))
+    readCount = db.Column(db.Integer)
+    buyCount = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Goods {}>'.format(self.name)
+
+
+class GoodsType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(20))
+
+    def __repr__(self):
+        return '<GoodsType {}>'.format(self.type)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.String(20))
+    pwd = db.Column(db.String(20))
+    username = db.Column(db.String(50))
+    sex = db.Column(db.Boolean)
+    address = db.Column(db.String(1000))
+    email = db.Column(db.String(50))
+    telephone = db.Column(db.String(100))
+    mobile = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
