cf6bbe3d63acca356a8fb05bf9800481c637485a
esqning <esqning@aliyun.com>
2021-03-24 10:27:19 +0800

    添加购物车基础页面模型

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,5 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('cart.html', title='home', posts=posts)
+    return render_template('cart.html', title='home')
 
